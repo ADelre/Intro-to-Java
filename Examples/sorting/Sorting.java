dfee1e35b6bba115cf660cf550c6569e651ecc2f
Anthony Delre <AnthonyDelre@outlook.com>
2023-12-18 02:38:18 -0500

    Corrected syntax error

--- Examples/sorting/Sorting.java
+++ Examples/sorting/Sorting.java
@@ -3,7 +3,7 @@ package Examples.sorting;
 public class Sorting {
 
     private static int[] sortedNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    private static int[] unsortedNumbers = {10, 9, 8, 7 ,6 ,5 ,4, 3 ,2 ,1}
+    private static int[] unsortedNumbers = {10, 9, 8, 7 ,6 ,5 ,4, 3 ,2 ,1};
 
     public static void main(String[] args) {
     }
